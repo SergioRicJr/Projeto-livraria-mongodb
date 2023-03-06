ec6741eb2828e440dc70f217be7a353117256b84
Sergio <sergioricjr7@gmil.com>
2023-03-05 22:27:39 -0300

    função adicionar livro adicionada

--- public/js/adicionarLivros.js
+++ public/js/adicionarLivros.js
@@ -1,15 +1,35 @@
 var btn = document.querySelector("button")
+var select = document.querySelector("#genero")
+var inputTitulo = document.querySelector("#titulo")
+var inputAutor = document.querySelector("#autor")
 
-async function criando() {
+btn.addEventListener("click", async ()=>{
+    var titulo = inputTitulo.value
+    var autor = inputAutor.value
+    var genero = select[select.selectedIndex].value
+
     await fetch("http://localhost:3000/livros", {
         method: "POST",
         headers: {"Content-type": "application/json"},
         body: JSON.stringify({
-                "titulo": "Clean Code",
-                "autor": "Robert C. Marin",
-                "genero": "Informática"
+                "titulo": titulo,
+                "autor": autor,
+                "genero": genero
                 })
     })
-}
+})
 
-btn.addEventListener("click", criando)
+
+// async function criando() {
+    // await fetch("http://localhost:3000/livros", {
+    //     method: "POST",
+    //     headers: {"Content-type": "application/json"},
+    //     body: JSON.stringify({
+    //             "titulo": "Clean Code",
+    //             "autor": "Robert C. Marin",
+    //             "genero": "Informática"
+    //             })
+    // })
+// }
+
+// btn.addEventListener("click", criando)
